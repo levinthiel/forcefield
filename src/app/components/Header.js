0cb414acd3a3d4cf9d4c398ebf1f613156232060
levin <levinthiel@proton.me>
2025-04-01 10:12:40 +0200

    add responsiveness to header

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -54,28 +54,30 @@ export default function Header() {
             </LogoIcon>
             <SubTitleContainer>
                 <SubtitleDeco>
-                    <div>
+                    <WarningTextContainer>
                     <WarningText>
                         WARNING
                     </WarningText>
-                    <Image
-                        src="/fake-text.svg"
-                        alt="Fake text"
-                        width={198}  // Set the width
-                        height={138} // Set the height
-                        layout="responsive"
-                    />
-                    </div>
-                    <Image
-                        src="/alert-icon.svg"
-                        alt="alert-icon"
-                        width={137}  // Set the width
-                        height={121} // Set the height
-                        layout="responsive"
-                    />
+                        <Image
+                            src="/fake-text.svg"
+                            alt="Fake text"
+                            width={146}  // Set the width
+                            height={167} // Set the height
+                            layout="responsive"
+                        />
+                    </WarningTextContainer>
+                    <WarningIconContainer>
+                        <Image
+                            src="/alert-icon.svg"
+                            alt="alert-icon"
+                            width={137}  // Set the width
+                            height={121} // Set the height
+                            layout="responsive"
+                        />
+                    </WarningIconContainer>
                 </SubtitleDeco>
                 <Subtitle>
-                    <p>Force Field: Chronicles from the Edge</p>
+                    <p>Chronicles from the Edge</p>
                     <p>Science fiction short stories</p>
                 </Subtitle>
             </SubTitleContainer>
@@ -97,10 +99,14 @@ const TopBar = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: end;
-    padding: 20px;
-    min-height: 80px;
+    padding: 0px 0 10px 0 ;
+    min-height: 60px;
     background: var(--orange);
     color: var(--black);
+
+    @media (max-width: 1000px) {
+        display: none;
+    }
 `;
 const LogoText = styled.div `
     background: transparent;
@@ -111,55 +117,108 @@ const LogoText = styled.div `
     font-size: 54px;
     font-weight: 900;
     font-family: outfit;
+
+    @media (max-width: 1335px) {
+        font-size: 40px;
+    }
+    @media (max-width: 1000px) {
+        font-size: 30px;
+        width: 100%;
+    }
+    @media (max-width: 400px) {
+        font-size: 18px;
+        width: 100%;
+    }
 `;
 const LogoTextBars = styled.div`
     display: flex;
     flex-direction: column;
     gap: 10px;
+
+    @media (max-width: 400px) {
+        gap: 3px ;
+    }
 `;
 const LogoTextBar1 = styled.div`
     width: 100%;
-    height: 5px;
-    border-radius: 7px;
+    height: 2px;
     background: var(--orange);
+
+    @media (max-width: 400px) {
+        height: 1px;
+    }
 `;
 const LogoTextBar2 = styled.div`
     width: 100%;
-    height: 9px;
-    border-radius: 3px;
+    height: 5px;
     background: var(--orange);
+
+    @media (max-width: 400px) {
+        height: 2px;
+    }
 `;
 const LogoTextBar3 = styled.div`
     width: 100%;
-    height: 18px;
-    border-radius: 5px;
+    height: 14px;
     background: var(--orange);
+
+    @media (max-width: 400px) {
+        height: 4px;
+    }
 `;
 const LogoTextBar4 = styled.div`
     width: 100%;
-    height: 38px;
-    border-radius: 7px;
+    height: 23px;
     background: var(--orange);
+    @media (max-width: 400px) {
+        height: 10px;
+    }
 `;
 const LogoTextBar5 = styled.div`
     width: 100%;
-    height: 66px;
-    border-radius: 7px;
+    height: 45px;
     background: var(--orange);
+    @media (max-width: 400px) {
+        height: 17px;
+    }
 `;
 const LogoIcon = styled.div`
     width: 20%;
     border-right: 4px solid var(--orange);
-    padding: 40px;
+    padding: 10px 60px;
+    max-height: 305px;
+
+    @media (max-width: 1000px) {
+        display: none;
+    }
 `;
 const SubTitleContainer = styled.div`
     width: 20%;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+
+    @media (max-width: 1000px) {
+        width: auto;
+        display: block;
+        height: 100%;
+        flex-grow: 1;
+    }
 `;
 const SubtitleDeco = styled.div`
     padding: 10px;
+    display: flex;
+    gap: 10px;
+
+    @media (max-width: 1335px) {
+       display: none;
+    }
+`;
+const WarningTextContainer = styled.div`
+    width: 50%;
+`;
+const WarningIconContainer = styled.div`
+    width: 50%;
 `;
 const WarningText = styled.p `
     font-weight: 900;
@@ -175,4 +234,8 @@ const Subtitle = styled.div `
     background: var(--orange);
     color: var(--black);
     padding: 10px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
 `;
