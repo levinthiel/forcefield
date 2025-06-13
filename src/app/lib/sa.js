55a21c6d72ce0943e3316dc34bb9286638be476a
levin <levinthiel@proton.me>
2025-06-13 21:00:48 +0200

    styling and formating of story in markdown

--- src/app/lib/sa.js
+++ src/app/lib/sa.js
@@ -1,18 +1,239 @@
 const sagasu = [{ 
 	id: "sagasu",
-	title: "Sagasu incident",
+	title: "The Sagasu incident",
 	smallcoverpath: "./sagasu/sagasu-s.png",
 	bigcoverpath: "/sagasu/sagasu-l.png",
-	time: "5 min",
+	time: "18 min",
 	tags: "Space Horror / First Person",
 	storyContent:	`
 
-	**Taike-Mosabe Company Cargo ship**, _Sagasu_  
-80 man crew  
+---
+
+*Taike-Mosabe Company Cargo ship, Sagasu*
+
+80-man crew  
 On its way back to Earth  
 ETA: 48 hours  
 
----`,
+---
+
+You jump awake as the blaring sound of the alarm pounds your eardrums. At the same time, you realize that your quarters are bathed in the pulsating red light that always accompanies the never-ending sound of the Emergency alarm.
+
+*“Ok pal, this is not a drill,”* you think to yourself. *“Stay sharp!”*
+
+On opening your door, you realize that, in the corridor, all the people are running towards Command or at least to the Great Hall. In a trot, you decide to follow.
+
+People rush past you with little to no concern on their face. This relaxes you a bit and at that moment, you realize how tense you have been since you opened your eyes. You try to ask some of your crewmates what’s going on, but they just shrug their shoulders or flat out ignore you.
+
+*“What a great way to start the day! Those company people are all the same… bloody money-driven robots! No questions asked!”*
+
+In the Great Hall everybody is listening to the captain of this asteroid-hauling tin can.
+
+> “Listen up people, about 10 minutes ago the proximity alert went off up in Command, we first thought it might be another cargo that’s gone off course. But it seems to be something else, and it’s big.”
+
+Following this statement there are a few seconds of silence. Then the crowds start murmuring excitedly.
+
+Your own thoughts go ballistic. *“Who is this and what do they want? Pirates maybe? All this excitement before coffee… this must be a Thursday!”*
+
+> “What kind of something?” exclaimed a voice at the back of the room, who seemed to ask what everyone was thinking.
+
+> “Well… it’s big, very big indeed. About 10 times our size. It’s sending signals our way, and we’re working on understanding them. So far, ‘they’ don’t seem threatening.”
+
+> “They? Who the fuck are they?! Pirates?”
+
+> “No, it isn’t something we encountered before, it’s too dark out there to see it with the naked eye, but it’s on our radars…”
+
+Behind the captain a door bursts open, and a technician comes running in.
+
+> “Sir! Sir, the readings are off the charts, and we can’t get a signal to Earth, or…” — he wipes the sweat off his brow — “to anyone for that matter. Nothing in or out. We’re completely cut off!”  
+
+His voice quivers as he looks questioningly at the captain for further orders.
+
+> “Ok let’s all remain calm and let’s—”
+
+Before the captain is able to finish, there is a loud crackling that resonates through the hall. You look around to locate the source of the sound, but it doesn’t seem to come from anywhere, or rather it is coming from everywhere at once. The crackling changes, transforming into a cacophony of incomprehensible voices. Then, as abruptly as it came, it’s just gone.
+
+This is when you realize that the alarm is not blaring anymore, there is just silence.
+
+Your mind races. *“Why is it so quiet? When did the alarm stop? When did the lights change back to normal?”*
+
+> “What the fuck was that?!” asks a tall and brawny security officer, his voice shrill with fear.
+
+To answer him the crackling returns, different this time — the sound of voices is unmistakable.
+
+> “Klrrrrrrrt– We are Mochán– klrrrt– we are waiting you– klrrrrrrrt kkkkt– Come to us…”
+
+This voice, just as the sound before, comes from everywhere, and you could swear that you hear it even inside your head. It’s like your own voice when you are thinking, when your brain is talking to itself.
+
+The crowd becomes panicked. It takes 10 minutes before the captain lets off a light flare and order is restored. The technician from before returns to say they have control of the instruments again and have managed to trace the signal back to that very big something.
+
+*“Wow, guys, I don’t need a whole bunch of blinking lights to tell me that those voices were coming from there. Thanks, Sherlock!”* you think sarcastically.
+
+---
+
+In the mess hall a few hours later you overhear two officers talking in hushed voices.
+
+> “They managed to create a communication channel with our new ‘friends’. The captain took 11 men, all from Taike Security, and went in a pod to meet those ‘Mochán’.”
+
+*Jar heads,* you think, shaking your head. *“Shouldn’t we call home before we attempt anything, maybe call for reinforcements?”* — your anger is tempered by a sense of doubt — *“But what do I know about human-alien relations? The only alien I know is myself.”*
+
+You take a deep breath of the stale ship air. *“Ahh… this filtered air has nothing on the sweet air back on Earth. We’re so close to home, so close to that big blue polluted rock.”*
+
+A sense of dread fills you, and you wonder if you will ever taste that sweet air again. *“If you ask me, we should wait before we do anything involving those voices.”*
+
+Later, you attempt to clear your mind with a workout and a hot shower. But the sense of dread remains. Laying down in your bunk, which is just a little bit too short, you grab your music player. You put it to your head and instantly music starts playing, and quickly you fall into a deep sleep.
+
+---
+
+*Taike-Mosabe Company Cargo ship, Sagasu*
+
+80-man crew  
+ETA to Earth: 32 hours  
+
+---
+
+This time it’s not the alarm that wakes you up, but the sound of the blood pulsating in your head. The constant in your temples is accompanied by a massive headache.
+
+*“Damn it! The last time my head hurt this bad was when I went out to celebrate getting this bloody job, a few months ago!”*
+
+You get up slowly and sit on your bed, holding your head in one hand. Then you remember that you had some kind of freakish nightmare, and a voice of some kind, but the more you try to remember what actually happened, the more the dream seems to fade away, and your head hurts even more.
+
+*“Ok let’s see if those morons are back from their little gathering with our new friends.”*
+
+The ship seems awfully quiet, but then it's better that way because your head is still killing you. Wandering around, there is no one, everybody seems to be gone.
+
+*“What the hell is going on here…where is everybody?”* The pounding in your head is picking up pace.
+
+Just when you thought you were completely alone on this ship, you see one of the Taike Security guys through the door to the gym. You feel relieved, and you start to run to him, but your splitting headache intensifies on the first step. You bend down with your hands on your knees as you try not to throw up or faint. Taking deep breaths until the pain returns to a dull ache, you stagger over to your bench-pressing crewmate, who hasn’t seemed to have noticed you at all.
+
+> “Hey buddy, how was the tour with the ‘Mochán’ did you—”
+
+But the words aren’t coming out of your mouth. Looking at the guy, you just realize he’s covered in blood. His hands are crimson, his left eye is blackened and swollen shut, the other one completely bloodshot, his lower lip is cut, and his uniform is wet with blood. He continues bench pressing and ignores you.
+
+> “Hey dude? Are you okay? It looks like you guys run into trouble, huh?”
+
+You let out a nervous laugh. Still nothing. A tingle goes down your spine, and you decide your blood doesn’t need to be on this guy’s hands too. You slowly back away through the door without letting the battered exercising crazy person out of your sight.
+
+Out of the gym, you close the door cautiously, you grab the chair that was just next to it and block the gym door as silently as possible, then you turn around and start running like hell. In that nightmarish encounter you totally forgot your headache, but it comes straight back to you, making you fall to your knees in pain.
+
+You try to catch your breath thinking *“What in the hell was that? What happened to that guy? Did the aliens do this? Where the fuck are the others?”*
+
+Overcome by your headache that feels like your skull is shrinking, fast, and your eyes have become too big for their sockets, you lay on the cold metal floor. You can hear your blood pulsing through your temples and your rapid breathing is beginning to echo inside your cranium. You can see the condensation from your breath on the floor before everything goes black.
+
+---
+
+*Taike-Mosabe Company Cargo ship, Sagasu*
+
+ETA to Earth: 31 hours  
+
+---
+
+> “We could not reach you. Hidden. How did you? Tell us. Tell us! We must know!”
+
+The voices from the night before bellow inside your head. Suddenly, you wake up and sit straight up while screaming:
+
+> “Get the fuck out of my head!”
+
+Very slowly, you get on your feet, steadying yourself with one hand on the wall.
+
+*“Damn, those aliens probably did something to that guy in the gym just like they tried with me… but why didn’t it work? Maybe others managed to resist them somehow too. I’ve got to look for the others.”*
+
+Half-blinded by your pain you resume your wandering in the narrow corridors of the Sagasu.
+
+After what feels like an eternity you reach the mess hall. The doors are closed but through the glass you can see another Taike Security employee sitting at the table, his back to you. He is shirtless, there are countless open wounds visible on his back dripping with blood, and he has what appears to be half a plate embedded into his shoulder. Yet he is eating normally, completely unaffected by his injuries.
+
+*“Are they not feeling pain? Is that the big guy from earlier? What the fuck is wrong with them?”*
+
+Half-blinded by your still throbbing migraine, you peer through the glass to see if someone else is with him. On the next table there is a body of a man lying on his back with a tray sticking out of his rib cage. In the back of the room another body is slouched against the wall. The head is gone, just a bloodstain exploded onto the wall.
+
+*“What the…? What? How?”*
+
+Trembling you double over and vomit all over the floor and your shoes.
+
+*“Shit! I hope this fucker didn’t hear me.”*
+
+Not taking chances you start running down the corridor, to get as far away as possible from those freaks. Your headache is still intense, and you have to take short breaks in order for your vision to come back and the pain to subside.
+
+Over the loudspeaker comes an automated announcement:
+
+> “Core override procedure commencing, please follow the emergency procedures and make your way to the escape capsules.”
+
+*“Oh c’mon! Really?! This must come from Command, so that means there’s somebody up there! I hope they are sane and can get me the fuck out of here.”*
+
+The voice repeats:
+
+> “Core override procedure commencing, please follow the emergency procedures and make your way to the escape capsules.”
+
+A few minutes later, you reach the Command corridor. The lights don’t seem to be working, and the corridor is pitch black.
+
+*“Typical…”*
+
+You can hear the loud clanking of the door at the end. A first aid kit is blocking it from sliding completely shut. Carefully you creep forward and try to see what is happening inside through the illuminated slit.
+
+> “Core override procedure commencing, please follow the emergency procedures and make your way to the escape capsules.”
+
+Inside the technician from yesterday is pushing buttons frantically and muttering to himself.
+
+> “Hey buddy, what’s going on? Did you just start the overriding procedure?” you shout through the crack in the door.
+
+The tech guy jumps in fright and looks at you horrified.
+
+> “I… I had to.” — his voice wavering — “this is the only way… I … they have all gone crazy, I think they got to me too…”
+
+He stares blankly like he is never going to blink again. He starts pacing back and forth.
+
+> “What do you mean they got to you?”
+
+> “My head… it hurts so much… and the voices… they keep asking me questions and telling me what to do… Everyone is dead! I feel like I haven’t slept in days.”
+
+> “Ok calm down, I think we can help each other here, nobody needs to do anything crazy.”
+
+He jumps towards the door and falls on all fours just two meters in front of you. He looks at you with his bloodshot unblinking eyes and says hoarsely:
+
+> “Nobody can be helped. We can’t let them… I think I need some sleep! We can’t let them!”
+
+> “Core override procedure 100%. The core will detonate in 20 seconds.” 
+
+The alarm starts to blare, and a red flashing light illuminates the dark corridor.
+
+> “No! No, no, no, no time for sleeping! Hey, Stop the ship from exploding, you fucking nut job!”
+
+You try to frantically push your way through the slit in the door to get to the controls. The technician looks at you horrified, his eyes bulging.
+
+He screams at the top of his lungs:
+
+> “This way no more voices! This way I can maybe rest for a while!”
+
+> “Are you insane, this way we will all die, no more ship, nothing! Open this damn door and let me stop the ship from blowing up!”
+
+> “No! They want me to… it’s the only way…this is how my head will—”
+
+> “Core override procedure 100%. The core will detonate in 5 seconds.”
+
+You try to wiggle through that door, but your belly, developed from the frequent trips to the mess hall, is preventing you from passing.
+
+> “4 seconds.”
+
+You’re trembling from panic, your whole body is shaking madly, stuck between the corridor and command.
+
+> “3 seconds.”
+
+Refusing to accept the inevitable, you push with all your might. You feel the door giving way. The technician has started ripping out his hair, kneeling on the floor looking up at the ceiling.
+
+> “2 seconds.”
+
+He looks at you with a crazy smile on his face, blood drips from in his eyes and scalp. In a low voice that you still can hear over the alarm he says:
+
+> “This is how they do it, always, the Mochán, this is how they get everyone!”
+
+> “1 second—”
+
+---
+
+*Taike-Mosabe Company Cargo ship, Sagasu*
+
+ETA to Earth: No entry
+`,
 
 },];
 
