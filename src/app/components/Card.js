67ca2278c85e5c61dab41c119becdacf519cb329
levin <levinthiel@proton.me>
2025-07-03 20:10:38 +0200

    add industrial espionage story, adjust card styling

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -19,19 +19,20 @@ export default function Card({storytitle,storyReadingTime,storyTags, storyCoverP
 }
 
 const CardWrapper = styled.section`
-  background: var(--beige);
-  display: flex;
-  border: 2px solid var(--beige);
-  border-radius: 7px;
-  color: var(--beige);
-  width: 100%; /* Full width of grid cell */
-  height: 100%;
+    background: var(--beige);
+    display: flex;
+    border: 2px solid var(--beige);
+    border-radius: 7px;
+    color: var(--beige);
+    width: 100%; /* Full width of grid cell */
+    height: 100%;
 `;
 const CardCover = styled.div `
     width: 145px;
     background: ${({ $smallcoverpath }) => $smallcoverpath ? `url(${$smallcoverpath})` : 'none'} var(--beige);
     border-radius: 7px 0 0 7px;
     border-right: 2px solid var(--beige);
+    background-size: cover;
 `;
 const CardBodyWrapper = styled.div`
     width: 100%;
--- src/app/lib/allTheStories.js
+++ src/app/lib/allTheStories.js
@@ -1,9 +1,10 @@
 import sagasu from "./sagasu";
 import priestess from "./priestess";
 import passengers from "./passengers";
+import industrial from "./industrial"
 /* import story2 from "./stories/story2";
 import story3 from "./stories/story3"; // Add as many as you have
  */
-const stories = [...sagasu,...priestess,...passengers, /* story2, story3 */];
+const stories = [...sagasu,...priestess,...passengers,...industrial /* story2, story3 */];
 
 export default stories;
