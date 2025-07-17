e7075170b588058c9b5c365dddc214962b5f73d3
levin <levinthiel@proton.me>
2025-07-17 19:50:43 +0200

    add tunnel story, adjust footer styling

---
+++ src/app/lib/tunnels.js
@@ -0,0 +1,167 @@
+const tunnels = [{ 
+	id: "tunnels",
+	title: "Tunnels",
+	smallcoverpath: "./tunnels/tunnels-s.png",
+	bigcoverpath: "/tunnels/tunnels-l.png",
+	time: "16 min",
+	tags: "Adventure / Mystery",
+	storyContent:	`
+My name is Pierce, I turned twelve last winter. 
+
+Pretty incredible things have happened to me lately. By Incredible I mean “not believable”, so crazy and unbelievable that if I were to tell my story to anyone I know, it would seem so far-fetched that they would think I lost my mind.
+
+But I have to tell someone! So, whoever you are, here is my story.
+
+It all started about a full moon ago, I was walking from Celtgish castle to my parents’ home. The sun was going down, but if I walked quickly enough, I would manage to arrive there before dark. 
+
+I was walking thought the forest when I saw a rabbit hopping across the path, I stopped, hoping it wouldn’t see me. “If I come home with a rabbit, mother and father will finally think I’m not as skinny and useless as I look” I thought. Yeah, I get bullied at the castle and by my family. That’s the life of a twelve-year-old skinny squire, I guess.
+
+The stupid rabbit did see me, got scared and started hopping hastily along the path. I decided to chase it and was determined to catch it. I didn’t let it out of my sight, I was focusing only on the jumping white tail right in front of me, running at full speed. I was so close, I stretched my hand out… but my leg got caught in a root sticking out of the ground. As I fell forward, I slowed my tumble with my hands out in front of me. I momentarily lost my breath. At that moment, I heard the rabbit’s small screech and jerked my head up to look at what happened. The small animal was gone, the path was clear, and there was no sign of movement. 
+
+I sighed and got to my feet, checked my hands. They were hurting a bit and I could see scratches here and there, but I was fine. After I cleaned the dust from my clothes, I thought to myself “It’s still skinny and useless Pierce then...”.
+
+When I was ready to go on, I had a quick look over my shoulder at where the sun was. I still had time before dark. I turned around and from the corner of my eye I thought I saw movement near a tree on the side of the path. Could it be that rabbit again? 
+
+But when I took a step closer to look at what it was, nothing was moving, yet it looked like something was moving at the same time, like a buzzing.
+
+Right there in front of me, it was like the air was shimmering. Like the air above a fire or a steaming pot of stew, but bigger. A surface about the size of a door, the air was breathing and bubbling. I could still see through it, but everything was distorted, like on the surface of a lake.
+
+I was paralysed, transfixed, almost bewitched by this phenomenon. For a few seconds, I couldn’t take my eyes off of it. But this couldn’t mean anything good, so I shook myself and started running as fast as I could. I wanted as much distance between me and that... whatever it was.
+
+My legs were burning, my throat hurting, my pulse was thumping in my ears, but I didn’t stop. I couldn’t, fear was propelling me forwards, I just wanted to be home.
+
+My brother always told stories about witches in that forest to scare me when I was little, but I never really believed him. It was known that some people disappeared there, but I think they just got eaten by a bear or drowned when drunk, and fell in the swamp or something. 
+
+I was trying still trying to comprehend what I just saw while running. Trying to calm the pain I felt between my ribs. After a while I slowed down and from afar I could see the chimney smoke from the house. I was saved! 
+
+I finally came home and tried to explain all this to my family, but father and my brother Brom kept making fun of me. They told me that I always try to tell stories to mask the fact that I can’t do anything else, not even catch a simple rabbit. 
+
+As it always was, during the two days each moon that I can go back home from the castle, I was told how useless I was. “Not fierce Pierce” they call me, and this caught on at the castle as well. Yay me!
+
+The morning after the two days at home, Brom and I were on our way back to Celtgish. I found that place where the rabbit vanished but the bubbling air was gone – of course – that pushed my brother to make silly faces at me and sing something along the lines of “not fierce Pierce, the useless squire, to be a man he still aspires....”
+
+So my life went on, as did the bullying, and the days passed. 
+
+But one evening a few weeks later, while my brother was training his sword moves, something happened, and he accidentally started an enormous fire in the courtyard. I was working in the stables when I heard shouts and cries for help. I went out to see what was going on, and the whole equipment barn was on fire and I could see Brom in the middle of the yard paralysed and in total disbelief of what just happened. 
+
+When I called him, he turned around and came running to me. He was panicking and told me that if they found out who had done this, we would both be in the greatest trouble and that we would probably rot in the Dungeons for the rest of our lives.
+
+After a second of hesitation, and with the will to stay free and evade the whip, we decided to run. It wasn’t too late in the evening, so the Castle doors were still open, allowing us to flee.
+
+We weren’t even running for long when behinds us we could hear the dogs barking and men shouting orders. They were all chasing us. 
+
+Panic overwhelmed us both, we looked at each other and without a glance back kept running for our lives. Before we realized, we had reached the forest. Brom told me that we needed to split up and look for cover. I tried to convince him that I will be caught if he leaves me on my own. Shouting at me, he tried to convince me that this was the only way for us to escape. I kept glancing over my shoulder to see if anyone was coming. I couldn’t see anything, and over his shouts and the deafening pulsating blood at my temples I couldn’t hear anything either.
+
+This is the part where this evening started to become so surreal, scary and crazy - I mean even more that it already was.
+
+Still running like the devil was on my tail, I suddenly realized that I was alone, and that Brom was nowhere to be seen. At that moment, the fear and panic reached a level I never experienced before. Sweat was dripping from my forehead into my eyes, making everything look blurry. My body felt like I was breathing fire, all my limbs were shaking as if I was on a cart running down a very bad road.
+
+I started shouting Brom’s name, which made my throat burn even more. I was turning in all directions. I could hear barking coming closer and closer. My heart was about to give up, I had lost my orientation completely and in a desperate attempt to stay alive I started dashing into the trees away from the dogs, who by now felt like they were all around me. 
+
+I didn’t even manage to run more than three strides, when I run into something. I realized too late that it was a wall of bubbling air, my speed kept me from slowing down, and I ran through the unknown surface. But instead of coming out the other side 
+
+my speed was stopped instantly, as if I had run into a wall of water. I felt pressure on every inch of my skin as if that surface was trying to shrink me, and at the same time a strange force was pulling me further and further, deeper and deeper into the unknown. 
+
+I had the feeling that I was being pulled apart from the inside. I couldn’t tell you how long this went on, it felt like it lasted for minutes. 
+
+By that time, I had forgotten the dogs and their masters pursuing us, forgotten Brom’s sudden disappearance. Everything was a whirlwind of horrible sensations. The whole time I kept my eyes tightly shut, I feared I would never recover from the madness if I dared to look around me.
+
+As suddenly as it started, the torture ended, and I fell face forward on all fours trying to catch my breath. After a few seconds, I realized that I wasn’t kneeling on the forest floor. Under me was a hard and smooth reflecting surface, bathed in a greenish light. It was then that I realized that people were shouting in front of me, I heard a lot of voices and raised my head, and what I saw will stay in my mind forever. 
+
+From my kneeling position I could see three figures dressed in long light green cloaks, their mouths and noses were covered by pieces of cloth. The three figures were fighting to keep another person still.
+
+“Pierce help me! Help me! Leave me alone, you bastards”.
+
+It then dawned on me that it was Brom, who was being dragged away by these three cloaked men. 
+
+Without hesitating, I got to my feet and jumped on the man closest to me. He saw me and let go of Brom to stop me, but I was too fast, and my momentum knocked him backwards. His head hit what looked like a table, and he was knocked out. This distracted the two other green men so that Brom managed to get one arm free and started punching wildly around him. I got to my feet and Brom was now free and standing by my side. We were panting like feral animals that had been cornered, we looked at each other and without a word turned around and jumped back through that frameless window into the unknown. The feeling was the same, pressure on my body, the feeling of being dismembered, eyes closed. Everything was over quicker this time, and I was holding Brom’s hand so tightly I could feel my blood leaving my extremities.
+
+Abruptly the hard greenish ground we left behind transformed into soft warm sand. 
+
+Still holding my brother's hand, I opened my eyes and could see Brom standing there, more than a head taller than me, watching the horizon open-mouthed. 
+
+We were standing on a beach, looking out at the ocean, the air was hot and moist and there seem to be a gigantic fire on the horizon. Almost as though there was a battle going on. We could hear rumbling in the distance as though the battle was a storm and lightning was striking whatever was floating far away in front of us. 
+
+All of a sudden, we heard a hum, and the unmusical remainder of what might be called a roar. This ear-splitting noise was growing in intensity, and we realized this was coming from over our heads. 
+
+Simultaneously, we looked up and what we saw just piled up on top of this indescribable insanity.
+
+Five gigantic metal birds were flying in a formation like geese at the end of the summer. Roaring like a thousand lions, soaring with incredible speed in the direction of the fiery storm on the horizon. 
+
+Brom turned to me with madness in his eyes, grabbed me by the shoulders and started shaking me.
+
+“What is this madness?!” he cried. Unable to open my mouth or look him in the eyes, tears started to fall down my face. I turned around and saw that the window was still there. 
+
+“Let’s try to go back home, Brom, please” I yelled. 
+
+I grabbed his hand again, and we ran through the shimmering portal again. 
+
+This time around we were through in the blink of an eye and the horrible crushing feeling was so short that it was almost trivial. 
+
+I also decided to keep my eyes open this time and try not to blink. But all I saw was darkness and flashes of blue light, like blood slowly falling into water. 
+
+Cool air hit our faces when we came out the other side. A pleasant evening breeze was in the air. 
+
+We were standing in knee-high grass and the landscape was dark except for the silver light of the moon. We stood in a calm clearing surrounded by thick trees. The sky was cloudless.
+
+“Are we back?” I asked panting.
+
+“I don’t know, I hope.” Brom replied between breaths. Both bending over, trying to calm our racing hearts, we decided to sit in the grass and recuperate from this nightmarish experience.
+
+“Do you think they’re still after us?” I wondered after a moment of silence.
+
+“I don’t think anyone could have followed us here. Where are anyway? And what in Merlin’s name is going on here? What was all that?” Brom replied with panic rising again in his voice. 
+
+“You didn’t believe me back when I told you about the bubbling air and the rabbit.”
+
+“Would you have believed me?” We both looked at each other and started to laugh from stress and exhaustion. 
+
+I fell backwards in the grass and watched the stars, lying on my back, trying to find answers and to figure out what just happened. 
+
+So now you know everything that happened until here and now, where I am still lying in the tall grass looking at the stars, with my brother. 
+
+I can’t wrap my head around all this, what were these places and these people, those metal birds? Did we just go to hell and back?
+
+I break the silence by saying, “Oh! Look a shooting star” pointing at the sky. Brom takes a moment to reply, and I can hear him getting up, “Pierce... that’s not a shooting star” he says slowly.
+
+I realize that there’s something wrong. Still looking at the glowing thing in the sky, concentrating to figure out what it is, and soon realize there are more of them. 
+
+“Those are fire arrows! They found us!” Brom screams in the night.
+
+We look around frantically, when we can hear a rumble in the distance in front of us. 
+
+“Pierce, we have to –“ but before Brom can finish his sentence, he yells out in pain and clutches his left shoulder. Sticking out of his torso I can see a very long thin piece of light wood, an arrow. 
+
+“Brom noooo” I cry rushing to him in total panic. 
+
+Fear starts to rise and overwhelm me for the hundredth time today. My brother falls to one knee, moaning in pain and crying. 
+
+“Pierce” he said between gritted teeth, “You have to break it, and we need to get to cover.” He slowly shows me where I have to break it. I assume this was part of his training at the castle. With trembling hands, I grab the arrow and trying not to hurt Brom I snap the arrow in two. He screams at the sky, and in response comes another scream from the line of tree at the end of the clearing. The distant rumble from earlier is now close enough to us to realize that there are at least a dozen horses galloping toward us.
+
+Suddenly, a dark rider jumps into the clearing. He is outfitted head-to-toe in shining black plated armour that seems to have a radiance of its own in the dim light of the moon. In one hand he holds the reins of his horse and in the other swinging over his head a long and thin silver sword. He’s scream at us and approaching with terrifying speed. In a matter of seconds, he is close enough for us to see his face. But there was no face, it looked more like distorted unmoving features of a fanged demon. The sight of it terrified me beyond anything, but in a flash I grab my still moaning brother and run the other way. I feel strangely calm, suddenly running in that tall grass surrounded by the most nightmarish and terrifying experience ever.
+
+As we’re running from this riding demon, I realize that the window is still there, and holding my moaning and delirious brother, we try to close the distance between us and the tunnel as quickly as possible. I can hear the dark rider getting closer and closer behind us. When we reach the window, I push Brom through and jump after him while I hear the demon yell something in an unknown tongue that sounded like “Anata wa ima shinu!”.
+
+The trip through the window is instantaneous by now and before my body was completely through, I could already see that on the other side. We land on some sort of metal stage with stairs leading down from it. Brom is lying there in front of me, panting and sweating from exhaustion and pain.
+
+I kneel at his side, and he looks at me with fear in his eyes and asks “Baby brother is this ever going to end?” I am speechless, I just look at him with tear filled eyes. 
+
+A voice echoes suddenly from the other side of the room, “They’re here! Quick bring the med-kit one of them seems to be injured!”.
+
+Realizing that these people don’t seem to be a threat, I yell, “Please help! We don’t know where we are, and my brother has an arrow in his shoulder, please save him!”
+
+In an instant, we’re surrounded by men and women tending to me and Brom. They’re flashing small lights in my eyes, and so many hands are checking me.
+
+“They were not supposed to arrive today, what’s going on?”
+
+“They’re too young, can this be them?”
+
+“The chief said the portal would not open for another month...”
+
+The exhaustion is too great at that moment and my body and my mind are so tired, I feel consciousness leaving me. My vision blurs and get dark. I just can make out a deep masculine voice talking to me while I slowly become dead to the world, “Pierce, don’t worry, you’re safe now. Welcome to the future, kid.”
+
+`,
+
+},];
+
+export default tunnels;
