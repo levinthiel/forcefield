a9118fdd0bc2c02fd699929de8b2eb3477ab3d8f
levin <levinthiel@proton.me>
2025-11-18 21:27:15 +0100

    Update the card design, add gridbackground and add meta data

--- src/app/components/Card.js
+++ src/app/components/Card.js
@@ -1,10 +1,15 @@
 import styled from "styled-components"
 
 export default function Card({storytitle,storyReadingTime,storyTags, storyCoverPath }) {
-    console.log("cover path:", storyCoverPath);
     return (
         <CardWrapper>
-            {storyCoverPath && <CardCover $bigcoverpath={storyCoverPath} />}
+            {storyCoverPath && 
+                <CardCover $bigcoverpath={storyCoverPath}>
+                    <ImageOverlayDots></ImageOverlayDots>
+                    <ImageOverlayGradient></ImageOverlayGradient>
+                </CardCover>
+            }
+
             <CardBodyWrapper>
                 <CardTitle>{storytitle}</CardTitle>
                 <CardBody>
@@ -12,6 +17,11 @@ export default function Card({storytitle,storyReadingTime,storyTags, storyCoverP
                         <p>{storyTags}</p>
                     </CardTags>
                     <CardReadingTime>reading time: {storyReadingTime}</CardReadingTime>
+                    <CardMetaData>
+                        <small>V1.0.0</small>
+                        <small>18112025</small>
+                        <small>Skltrn</small>
+                    </CardMetaData>
                 </CardBody>
             </CardBodyWrapper>
             </CardWrapper>
@@ -19,23 +29,44 @@ export default function Card({storytitle,storyReadingTime,storyTags, storyCoverP
 }
 
 const CardWrapper = styled.section`
+    width: 100%;
+    height: 100%;
     display: flex;
     flex-direction: column;
     color: var(--beige);
-    width: 100%;
     cursor: pointer;
+    transition:all .3s;
+
+    &:hover{
+        opacity:0.8;
+        box-shadow: 0px 0px 40px rgba(22, 22, 22, 0.4);
+    }
 `;
 
 const CardCover = styled.div`
     width: 100%;
     aspect-ratio: 2 / 3;  /* Portrait format */
     border: 2px solid var(--beige);
-    background: ${({ $bigcoverpath }) => $bigcoverpath ? `url(${$bigcoverpath})` : 'none'}
-    center center / cover;
+    background: ${({ $bigcoverpath }) => $bigcoverpath ? `url(${$bigcoverpath})` : 'none'} center center / cover;
     border-radius: 7px;
+    position:relative;
+`;
+const ImageOverlayDots = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background: rgba(0, 0, 0, 0) radial-gradient(rgba(255, 255, 255, 0.212) 10%, transparent 1%);
+    background-size: 11px 11px;
+`;
+const ImageOverlayGradient = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    background: linear-gradient(0deg, rgba(180, 27, 6, 0.1) 0%, rgba(17, 17, 17, 0) 100%);
 `;
 const CardBodyWrapper = styled.div`
     width: 100%;
+    height: 100%;
     border: 2px solid var(--beige);
     border-radius: 7px;
     margin-top: 5px;
@@ -43,7 +74,6 @@ const CardBodyWrapper = styled.div`
     flex-direction: column;
 `;
 const CardBody = styled.div`
-    background: var(--black);
     padding: 16px;
     display: flex;
     flex-direction: column;
@@ -66,4 +96,13 @@ const CardReadingTime = styled.p `
     text-align: left;
     font-size: small;
     font-style: italic;
+`;
+const CardMetaData = styled.div`
+    display: flex;
+    justify-content: start;
+    font-weight: 400;
+    font-size: 12px;
+    gap: 10px;
+    margin-top: 15px;
+    opacity: 0.5;
 `;
