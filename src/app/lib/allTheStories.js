bce3a52fef77a547e06220e86b2bcc02f056065f
levin <levinthiel@proton.me>
2025-11-26 19:36:39 +0100

    add new story -survivor3

--- src/app/lib/allTheStories.js
+++ src/app/lib/allTheStories.js
@@ -5,9 +5,10 @@ import industrial from "./industrial"
 import tunnels from "./tunnels"
 import survivor1 from "./survivor1"
 import survivor2 from "./survivor2"
+import survivor3 from "./survivor3"
 /* import story2 from "./stories/story2";
 import story3 from "./stories/story3"; // Add as many as you have
  */
-const stories = [...survivor2,...survivor1,...tunnels,...industrial,...passengers,...priestess,...sagasu, /* story2, story3 */];
+const stories = [...survivor3,...survivor2,...survivor1,...tunnels,...industrial,...passengers,...priestess,...sagasu, /* story2, story3 */];
 
 export default stories;
---
+++ src/app/lib/survivor3.js
@@ -0,0 +1,138 @@
+const survivor3 = [{ 
+	id: "survivor3",
+	title: "Blood, Barns and Barbed Wire",
+	smallcoverpath: "./survivor3/survivor3-s.png",
+	bigcoverpath: "/survivor3/survivor3-l.png",
+	time: "15 min",
+	tags: "Zombie Apocalypse / Diary",
+	storyContent:	`
+
+---
+
+**Day 58 of the Apocalypse**
+
+*Weather: cloudy, smells like regret*   
+*Mood: nervous AF*   
+*Daily Mission: Save Josie*   
+*Main Mission: Don’t die*
+
+---
+
+
+I’m a big fucking idiot. I'm camped out in a busted out shack on a ridge in the woods, about a day away from Josie’s uncle's farm. I followed tracks the best I could until I landed here. I am sure she’s in there, and I am such an idiot that I think I can go in there and get Josie out. 
+
+I am peeking through a crack in the boarded-up window. About maybe 300 meters down, sits the Camp of horrors, a.k.a. Redneck Raccoon Ranch. Two battered farmhouses, one barn the size of a small aircraft hangar, and a homemade apocalypse fortress surrounding it all. It’s in the middle of the woods, far from the road, surrounded by rocks. In the woods, no one can hear you scream. 
+
+They’ve built double metal fences around the entire place. Between those fences? A trench. Not just any trench. A writhing, stinking, moaning trench full of zombies. It’s like a moat, but instead of water, it’s decomposing death. Creative. Effective. Completely Insane.
+
+So far I’ve seen three pickups parked inside the inner fence. At least two with those lovely hood ornaments — the ones with the moving, snarling, duct-taped and chained zombies on it. So I’m guessing… what? Six to ten guys? Maybe more? Maybe less? Still more than I want to tango with. More than I can handle. I’ll have to play it smart. So unbelievably smart, and Sir vives-by-luck will need to live up to his fucking name.
+
+Under the drawbridge-style gate, it is swarming with undead. Either they dump them there, or it’s a crap security flaw. I’m betting on the latter. I won't try the front gate, obviously. That would be suicide. I’ll go east, closest to my position, where the trench thins out, and the fence is less reinforced.
+
+Nightfall is in two hours. I’m going in when the world turns black and these bastards fall asleep on their guns.
+
+---
+
+**Night** 
+
+*Weather: cold*   
+*Mood: focused*
+
+---
+
+Got rid of the armour. It’s in a bush. If I don’t come back for it, it’s either looted or I’m zombie sushi. Either way, can’t sneak around sounding like a cutlery drawer.
+
+I crawled slowly down. Scratched my hands, I am full of mud and moss, and I’m cold. I feel like a super lame version of "Dutch" in Predator. Took me nearly 45 minutes to reach the fence. The trench here? Shallow. Only Hip high. I passed through chicken and barbed wire.  
+Only three zombies in the section. One is just a torso. The other two, just roaming around, snarling and jaw snapping here and there. Easy enough to distract or kill.
+
+I threw an old soup can into the ditch not too far from where I stood—clang\! All three groaned and shuffled toward it like toddlers after a candy bar. I stabbed the first one in the skull before he knew what was going on. Stabbed the second one before he turned around. Boom baby\!   
+Only the torso biter left. I decide to jump onto his head from the ledge of the ditch. He just disappeared into the mud with a muffled “flump”.  
+I look around, the way is clear. Nice\! 
+
+I was reading myself to climb out of the moat of death, when out of the darkness and the mud wall, a head snaps out and tries to bite me, then an arm.I nearly shat my pants — not literally — just forgot how to breathe for a second. I snapped and stabbed his scary face. Silence. Again.
+
+“Fuck me\!” I whisper to myself.
+
+Climbed out, split the fence open at a weak point. Rolled over, landed in a bush. Waited.
+
+Silence.
+
+I snuck between the barn and a tractor. Smelled like diesel and goat. A dog barked inside one of the houses—then nothing. No one came out.   
+My heart was going insane, banging against my muddy clothing. I could feel the pulse in my teeth. My breathing, calm. I was hyper focused. In the zone. Neo dodging bullets.
+
+I peeked through a window in the smaller farmhouse: four rednecks. Drunk, passed out. One of them was using a loaded rifle as a pillow. I fucking hate this planet.
+
+I silently reach the main house: two floors. Lights on upstairs. Josie’s in that house. I feel it in my gut. 
+
+I crept along the porch, stepping around beer cans and a dead squirrel. Front door creaked. My soul creaked louder. I waited. No movement, no sound. I moved like a fucking ghost\! Inside, the place reeked of sweat, tobacco, and the kind of masculinity you only find in monster truck commercials.   
+I looked around, there was light under a door near the kitchen. I glued my ear to the door. I could hear loud breathing and … was it the rattling from a chain?  
+I tried the door, it was locked. Damn it\! 
+
+I took a chance and whispered through the door, “Josie? You in there ?”. Instantly the rattling got frantic and a quiet “hm, hm” came out, confirming it.
+
+*Found her\!* 
+
+Josie.
+
+I found a plastic coaster and tried my luck picking the door. Sliding the coaster in between the door and door jamb. Wiggled for a while. I was sweating like a group of nuns at a cucumber farm. But then it clicked, and the door swung open.
+
+Tied to a radiator in the small room. Duct tape on her mouth. Face bruised. Still had fire in her eyes. And tears, dried ones and fresh ones.
+
+She saw me. Blinked slowly and sighed. The well known international signal for *thank fuck*. I motioned for silence. Took out my trusty Stabby Stabber, and sliced through the ropes. She winced, then hugged me so hard I thought my ribs would crack. Actually, I think one did.
+
+"Let’s get the hell out of here," I whispered.
+
+---
+
+**Day 59 of the Apocalypse 01:03**
+
+*Weather: zombie storm*   
+*Mood: let’s GOOOOO*
+
+---
+
+We snuck outside. Took a route between the buildings. Avoided a guy pissing off the porch. He never saw us.
+
+I stabbed the two zombies on the hoods because they were going bonkers at the sight of us.  
+Found the biggest truck. Keys inside. Of course. Why not? Welcome to insane Redneckland.
+
+We got in. Josie took the passenger seat, holding my lance like a damn war goddess. We drove through the main gate, through the draw bridge, and out of this shithole, like a bat out of hell. The car was a monster and didn’t even get more than a scratch.
+
+Half of the drawbridge fell behind us, in to the trench. The undead spilled out into the farm like floodwater, groaning and snarling. They poured into the camp like ants onto a sugar cube. Lights went on. Rednecks woke up screaming. Gunfire erupted. 
+
+Half naked, half asleep, crazy ass, drunk wildlings, were shooting everywhere and getting eating. If I didn’t just narrowly escape with my life, I had found it hilarious. It was such a bloody mess.
+
+We didn’t look back. We tore down the dirt road, the truck bouncing over potholes, guts on the bumper.
+
+Josie wooed. I did too.
+
+After a minute, she looked at me and said, "Took you long enough." and hugged me again, crying.
+
+"Had to get past your pet zombies first." I replied smirking.
+
+“Next time, *I* do the rescuing.” looking sternly in my direction. I just huffed in disbelief, and shook my head. This cost me a punch in the shoulder. It hurt like hell. We laughed.
+
+Behind us, the camp burned. Zombie buffet.
+
+Ahead: freedom. Sort of.
+
+“We’ve got half a tank of gas, a stolen truck, one Stabby Stabber, no food and nowhere to go” I said looking at the headlight lit road, and decided to keep “and each other” to myself.
+
+We stopped at a small waterfall we found on a hiking trail map inside the glove box. Cleaned the blood, the sweat and the mud off. Ate some jerky and a beer we found in the back. Living “la Vida Redneck”. 
+
+I was still pumped crazy with adrenaline. Still amazed that I, we, survived this clusterfuck. Sir vives-by-luck indeed, but also a little bit of skill as well. Right?   
+We warmed up in the truck, silent, and relieved. 
+
+I turned on the radio, and super loud country blues shook the whole car. She punched my shoulder again, but let the music on, just quieted it. 
+
+I closed my eyes and leaned back on the driver's seat. Exhaled loudly.   
+Josie’s alive.   
+I'm alive.  
+The world still sucks, but maybe, just maybe, we’ve got a chance.
+
+
+`,
+
+},];
+
+export default survivor3;
